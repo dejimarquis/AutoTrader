2c3bcb31b33601e30daa44afb80b4912639168be
Ayodeji Marquis <ayodejimarquis@gmail.com>
2021-03-19 14:37:56 -0700

    updating stock list iteration

--- src/Strategies/IntradayStrategy.py
+++ src/Strategies/IntradayStrategy.py
@@ -19,16 +19,17 @@ class IntradayStrategy:
         time.sleep(16*60)
         barsOfStocks = self.Market.getBarset(self.stock_list, self.barTimeframe, 1)
         qty_to_buy = self.Market.calculate_qty_to_buy(self.Account.getBuyingPower(), self.stock_list)
+        print("Amount of stocks to buy" + str(qty_to_buy))
         time.sleep(60)
 
         while not self.Market.aboutToClose():
             for stock in self.stock_list:
-                try:
-                    position = self.Account.getPosition(stock)
-                    print("We have a " + str(position.side) + "position in this current stock so skip")
-                    continue
-                except Exception as e:
-                    print(str(stock) + " " + str(e))
+                # try:
+                #     position = self.Account.getPosition(stock)
+                #     print("We have a " + str(position.side) + " position in this current stock so skip")
+                #     continue
+                # except Exception as e:
+                #     print(str(stock) + " " + str(e))
 
                 stockCurrentPrice = self.Market.getCurrentPrice(stock)
 
@@ -36,10 +37,14 @@ class IntradayStrategy:
                     stop_loss = {'stop_price': (barsOfStocks[stock][0].h + barsOfStocks[stock][0].l)/2}
                     take_profit = {'limit_price': stockCurrentPrice * 1.01}
                     self.Market.submitOrder(qty_to_buy[stock], stock, "buy", stop_loss=stop_loss, take_profit=take_profit)
+                    self.stock_list.remove(stock)
+                    break
                 elif stockCurrentPrice < barsOfStocks[stock][0].l:
                     stop_loss = {'stop_price': (barsOfStocks[stock][0].h + barsOfStocks[stock][0].l)/2}
                     take_profit = {'limit_price': stockCurrentPrice * .99}
                     self.Market.submitOrder(qty_to_buy[stock], stock, "sell", stop_loss=stop_loss, take_profit=take_profit)
+                    self.stock_list.remove(stock)
+                    break
 
             time.sleep(30)
 
